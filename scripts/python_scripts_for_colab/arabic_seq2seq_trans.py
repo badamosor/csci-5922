c209b7f659a29ca8cfa73a5ea54b83b0f57d8d7c
badamosor <badamosor@badamosor-vm-ubuntu.18.04>
2019-05-07 00:12:04 -0600

    Commented out mounting drive

--- scripts/python_scripts_for_colab/arabic_seq2seq_trans.py
+++ scripts/python_scripts_for_colab/arabic_seq2seq_trans.py
@@ -22,7 +22,7 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 SOS_token = 0
 EOS_token = 1
